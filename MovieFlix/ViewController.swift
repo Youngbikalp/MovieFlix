ba7b6c58bc4a56a8f862f0da9a0722e7b50bd96e
Bikalp <bikalp.timalsina@sjsu.edu>
2020-02-05 11:48:36 -0800

    Initial Commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Bikalp  Timalsina on 2/5/20.
+//  Copyright © 2020 Bikalp  Timalsina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
